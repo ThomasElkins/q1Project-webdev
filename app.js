5349257503dd071ce9128aad66a229f66e8aab1c
ThomasElkins <elkins.thomas@gmail.com>
2017-07-19 10:15:32 -0700

    lat/long working, images not

--- app.js
+++ app.js
@@ -3,10 +3,16 @@
 $(function(){
 
 $('#searchButton').click(function(){
-  var town = $('#city').val()
+  $('.clickable').empty();
+  var town = $('#city').val();
+  var distance = $('#range').val();
   $('#map').append('<iframe src="https://www.google.com/maps/embed/v1/place?q='+ town +'&key=AIzaSyBxPnXkJa0pzukRSn_d1xLRcZDXhEpDJxo" allowfullscreen></iframe>')
 
-$.ajax('https://trailapi-trailapi.p.mashape.com/?limit=6&q[activities_activity_type_name_eq]=hiking&q[city_cont]='+ town +'&radius=30', {
+$.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ town +"&key=AIzaSyAoIyj32avw0yFjL4PxPkuI5F6X3_V5jTQ", function(address){
+  var lat = address["results"][0]["geometry"]["location"]["lat"]
+  var long = address["results"][0]["geometry"]["location"]["lng"]
+
+$.ajax('https://trailapi-trailapi.p.mashape.com/?lat='+ lat +'limit=6&lon='+ long +'&q[activities_activity_type_name_eq]=hiking&q[city_cont]='+ town +'&radius='+ distance +'', {
   headers: {
     "X-Mashape-Key": "zUC9v7vhTTmshRZhrNE1rQk5JtbGp1SS6DujsnmsruSvj2RWYd",
     "Accept": "text/plain"
@@ -15,10 +21,14 @@ $.ajax('https://trailapi-trailapi.p.mashape.com/?limit=6&q[activities_activity_t
 }).then(function(data) {
   var hikeArray = data["places"];
   for (var i = 0; i < hikeArray.length; i++){
-    var injectName = hikeArray[i]["name"]
-    $('#resultsRow').append('<div class="col-md-2">'+ injectName +'</div>')
+    var injectName = hikeArray[i]["name"];
+    var injectDist = hikeArray[i]["activities"][0]["length"]
+    var injectImage = hikeArray[i]["activities"][0]["thumbnail"]
+    $('#resultsRow').append('<div class="col-md-2 clickable"><h4>'+ injectName +'</h4><h6>'+ injectDist +' <img src="'+ injectImage +'"/> ');
   }
   // End hike data function
+})
+// End lat/long function
 })
 // End click function
 })
